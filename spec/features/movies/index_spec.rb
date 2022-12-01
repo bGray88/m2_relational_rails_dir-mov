e200a93832a5e08f8b3a78d4bac2c886ca999f8e
Brandon Gray <brandonjoe88@hotmail.com>
2022-12-01 07:16:00 -0700

    feat: Adds child movies tests and actions

---
+++ spec/features/movies/index_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe 'Directors index' do
+  before :each do
+    @movie_1 = Movie.create!(version: 'US Release',
+                              rating: 'R',
+                              censored: false,
+                              length_in_mins: 94,
+                              name: 'Cronos')
+    @movie_2 = Movie.create!(version: 'Original Release',
+                              rating: 'R',
+                              censored: true,
+                              length_in_mins: 92,
+                              name: 'Cronos')
+    @movie_2 = Movie.create!(version: 'Original Release',
+                              rating: 'R',
+                              censored: false,
+                              length_in_mins: 106,
+                              name: 'The Devil\'s Backbone')
+  end
+
+  describe 'As a visitor' do
+    describe 'When I visit \'/movies' do
+      it 'shows each movie in the system including the movie\'s attributes' do
+        visit '/movies'
+
+        expect(page).to have_content(@movie_1.name)
+        expect(page).to have_content(@movie_1.version)
+        expect(page).to have_content(@movie_1.rating)
+        expect(page).to have_content(@movie_1.censored)
+        expect(page).to have_content(@movie_1.length_in_mins)
+        expect(page).to have_content(@movie_2.name)
+      end
+    end
+
+    describe 'When I visit \'/movies/:id' do
+      it 'shows the movie with that id including the movie\'s attributes' do
+        visit "/movies/#{@movie_1.id}"
+
+        expect(page).to have_content(@movie_1.name)
+        expect(page).to have_content(@movie_1.version)
+        expect(page).to have_content(@movie_1.rating)
+        expect(page).to have_content(@movie_1.censored)
+        expect(page).to have_content(@movie_1.length_in_mins)
+        expect(page).not_to have_content(@movie_2.name)
+      end
+    end
+  end
+end
