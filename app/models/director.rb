7462e91ba51caab5a8e8ca4c759fe962b464a112
Brandon Gray <brandonjoe88@hotmail.com>
2022-12-02 14:36:54 -0700

    fix: update model to validate created data

--- app/models/director.rb
+++ app/models/director.rb
@@ -2,6 +2,11 @@ class Director < ApplicationRecord
   has_many :movies
   default_scope { order(:created_at) }
 
+  validates :hometown, presence: true, allow_blank: false
+  validates :alive, presence: true, allow_blank: false
+  validates :age, presence: true, allow_blank: false
+  validates :name, presence: true, allow_blank: false
+
   def movie_count
     self.movies.count
   end
