f2bcf845855c8ca3c5ec368ef193441428373c05
Brandon Gray <brandonjoe88@hotmail.com>
2022-12-02 11:47:49 -0700

    refact: update for AR methods over ruby

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,6 @@
 class DirectorsController < ApplicationController
   def index
-    @directors = Director.all.sort_by { |director| director.created_at }
+    @directors = Director.all
   end
 
   def show
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,5 +1,6 @@
 class Director < ApplicationRecord
   has_many :movies
+  default_scope { order(:created_at) }
 
   def movie_count
     self.movies.count
