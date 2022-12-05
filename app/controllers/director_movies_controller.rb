91e6c2eb187f00088944e90d58d79363666c1dd7
Brandon Gray <brandonjoe88@hotmail.com>
2022-12-05 09:36:31 -0700

    feat: director movies list by length form

--- app/controllers/director_movies_controller.rb
+++ app/controllers/director_movies_controller.rb
@@ -3,5 +3,6 @@ class DirectorMoviesController < ApplicationController
     @director = Director.find(params[:director_id])
     @movies   = @director.movies
     @movies   = @director.movies.order(:name) if params[:sort] == 'true'
+    @movies   = @director.movies.where(length_in_mins: (params[:threshold].to_i + 1)..Float::INFINITY) if params[:threshold]
   end
 end
