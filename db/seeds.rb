590c301a30a8d7794aab58862e4196109bdaac29
Brandon Gray <brandonjoe88@hotmail.com>
2022-12-06 10:43:06 -0700

    update: populate more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-@director_guillermo = Director.create!(hometown: 'Guadalajara, Mexico',
-                                        alive: true,
-                                        age: 58,
-                                        name: 'Guillermo del Toro')
-@director_corman = Director.create!(hometown: 'Detroit, Michigan',
-                                    alive: true,
-                                    age: 96,
-                                    name: 'Roger Corman')
-@director_furie = Director.create!(hometown: 'Toronto, Canada',
-                                  alive: true,
-                                  age: 89,
-                                  name: 'Sidney J. Furie')
-@guillermo_movie_1 = @director_guillermo.movies.create!(version: 'Theatrical Release',
-                                    rating: 'R',
-                                    censored: false,
-                                    length_in_mins: 106,
-                                    name: 'Devil\'s Backbone')
-@guillermo_movie_2 = @director_guillermo.movies.create!(version: 'US Release',
-                                                        rating: 'R',
-                                                        censored: false,
-                                                        length_in_mins: 94,
-                                                        name: 'Cronos')
-@guillermo_movie_3 = @director_guillermo.movies.create!(version: 'Theatrical Release',
-                                                        rating: 'R',
-                                                        censored: true,
-                                                        length_in_mins: 92,
-                                                        name: 'Cronos')
-@corman_movie_1 = @director_corman.movies.create!(version: 'Theatrical Release',
-                                                  rating: 'UR',
-                                                  censored: false,
-                                                  length_in_mins: 72,
-                                                  name: 'Little Shop of Horrors')
-@furie_movie_1 = @director_furie.movies.create!(version: 'Viacom Release',
-                                                rating: 'PG',
-                                                censored: false,
-                                                length_in_mins: 94,
-                                                name: 'Superman IV: The Quest for Peace')
-@furie_movie_2 = @director_furie.movies.create!(version: 'Theatrical Release',
-                                                rating: 'PG',
-                                                censored: true,
-                                                length_in_mins: 90,
-                                                name: 'Superman IV: The Quest for Peace')
-@director_guillermo.created_at = @director_guillermo.created_at + 10 * 600
-@director_guillermo.save
+Director.create!([
+  {hometown: "Toronto, Canada", alive: true, age: 89, name: "Sidney J. Furie"},
+  {hometown: "Versailles, France", alive: true, age: 59, name: "Michel Gondry"},
+  {hometown: "Guadalajara, Mexico", alive: true, age: 58, name: "Guillermo del Toro"},
+  {hometown: "Detroit, Michigan", alive: true, age: 96, name: "Roger Corman"}
+])
+Movie.create!([
+  {version: "Theatrical Release", rating: "R", censored: false, length_in_mins: 106, name: "Devil's Backbone", director_id: 1},
+  {version: "US Release", rating: "R", censored: false, length_in_mins: 94, name: "Cronos", director_id: 1},
+  {version: "Viacom Release", rating: "PG", censored: false, length_in_mins: 94, name: "Superman IV: The Quest for Peace", director_id: 3},
+  {version: "Theatrical Release", rating: "PG", censored: true, length_in_mins: 90, name: "Superman IV: The Quest for Peace", director_id: 3},
+  {version: "Theatrical Release", rating: "R", censored: false, length_in_mins: 108, name: "Eternal Sunshine of the Spotless Mind", director_id: 4},
+  {version: "Theatrical Release", rating: "R", censored: true, length_in_mins: 96, name: "Cronos", director_id: 1},
+  {version: "Theatrical Release", rating: "R", censored: true, length_in_mins: 104, name: "Science of Sleep", director_id: 4},
+  {version: "Theatrical Release", rating: "R", censored: false, length_in_mins: 123, name: "Shape of Water", director_id: 1},
+  {version: "Theatrical Release", rating: "R", censored: false, length_in_mins: 94, name: "Suburbia", director_id: 6},
+  {version: "Theatrical Release", rating: "PG-13", censored: true, length_in_mins: 76, name: "Little Shop of Horrors", director_id: 6}
+])
